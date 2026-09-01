ba03a2f3aa64fb76e72edbc6abb7909da4b73c3e
ChuAn <1104594588@qq.com>
2026-09-01 22:02:53 +0800

    修改tips、新增配方

--- src/main/java/com/chuan/apothicflux/item/FluxSpawnerBlockItem.java
+++ src/main/java/com/chuan/apothicflux/item/FluxSpawnerBlockItem.java
@@ -1,6 +1,7 @@
 package com.chuan.apothicflux.item;
 
 import net.minecraft.ChatFormatting;
+import net.minecraft.client.gui.screens.Screen;
 import net.minecraft.network.chat.Component;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.BlockItem;
@@ -17,7 +18,12 @@ public class FluxSpawnerBlockItem extends BlockItem {
 
     @Override
     public void appendHoverText(ItemStack stack, Item.TooltipContext context, List<Component> tooltip, TooltipFlag flag) {
-        tooltip.add(Component.translatable("tooltip.apothic_flux.flux_spawner.auto_output").withStyle(ChatFormatting.GOLD));
+        if (Screen.hasShiftDown()) {
+            tooltip.add(Component.translatable("tooltip.apothic_flux.flux_spawner.auto_output").withStyle(ChatFormatting.GOLD));
+            tooltip.add(Component.translatable("tooltip.apothic_flux.flux_spawner.input").withStyle(ChatFormatting.GREEN));
+        }else {
+            tooltip.add(Component.translatable("tooltip.apothic_flux.flux_spawner.click").withStyle(ChatFormatting.GRAY));
+        }
         super.appendHoverText(stack, context, tooltip, flag);
     }
 }
